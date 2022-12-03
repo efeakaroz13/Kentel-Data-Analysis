5ac6ff1763843e96975761604a380e6880f93459
Efe Akaröz <69296379+efeakaroz13@users.noreply.github.com>
2022-12-03 21:47:31 +0300

    updates

--- dataget.cpp
+++ dataget.cpp
@@ -10,17 +10,57 @@ USAGE:
 #include <curl/curl.h>
 #include <nlohmann/json.hpp>
 #include <fstream>
+namespace
+{
+    std::size_t callback(
+            const char* in,
+            std::size_t size,
+            std::size_t num,
+            std::string* out)
+    {
+        const std::size_t totalBytes(size * num);
+        out->append(in, totalBytes);
+        return totalBytes;
+    }
+}
 
+using namespace  std;
 using json = nlohmann::json;
 int main(int argc, char *argv[]){
 	std::ifstream data("database.json");
-	json output= json::parse(data)
-	if (argc==6){
+	json output= json::parse(data);
+	if (argc==4){
 		std::string username = argv[1];
 		std::string ipaddress = argv[2];
 		std::string useragent = argv[3];
-		std::string city = argv[4];
-		std::string country = argv[5];
+		//std::string city = argv[4];
+		//std::string country = argv[5];
+		string url = "http://ip-api.com/json/"+ipaddress;
+
+		CURL* curl = curl_easy_init();
+		
+		curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+		curl_easy_setopt(curl, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
+		curl_easy_setopt(curl, CURLOPT_TIMEOUT, 10);
+		curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+		long httpCode(0);
+		std::unique_ptr<std::string> httpData(new std::string());
+		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, callback);
+		curl_easy_setopt(curl, CURLOPT_WRITEDATA, httpData.get());
+		curl_easy_perform(curl);
+		curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &httpCode);
+		curl_easy_cleanup(curl);
+		json allipdata = json::parse(*httpData.get());
+		string city = allipdata["city"];
+		string country = allipdata["country"];
+		json data = {{"username",username},{"ipaddress",ipaddress},{"useragent",useragent},{"city",city},{"country",country}};
+		output["views"].push_back(data);
+		cout<<output.dump(4)<<"\n";
+		ofstream fileout("database.json");
+
+		fileout<<output.dump(4);
+		fileout.close();
+
 		
 
 
